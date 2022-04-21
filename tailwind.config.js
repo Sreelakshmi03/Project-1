b1d14575f85273a96fc01c12ffe85220833941d3
Sreelakshmi V <sreelakshmivs371@gmail.com>
2022-04-21 16:26:27 +0530

    Tailwind css project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        'bounce': 'bounce 5s linear infinite',
+      }
+    },
+  },
+  plugins: [],
+  
+};
